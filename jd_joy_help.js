02cbdd32116eaf687cfd471c9b83ca2183def6d9
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-07-03 23:23:03 +0800

    Update jd_joy_help.js

--- jd_joy_help.js
+++ jd_joy_help.js
@@ -18,13 +18,18 @@ hostname = draw.jdfcloud.com
 ======================Surge=====================
 [Script]
 宠汪汪强制为别人助力= type=http-request,pattern=^https:\/\/draw\.jdfcloud\.com\/\/common\/pet\/enterRoom\/h5\?invitePin=.*(&inviteSource=task_invite&shareSource=\w+&inviteTimeStamp=\d+&openId=\w+)?&reqSource=weapp|^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/pet\/helpFriend\?friendPin,requires-body=1,max-size=0,script-path=jd_joy_help.js
+
 ===================Quantumult X=====================
 [rewrite_local]
 ^https:\/\/draw\.jdfcloud\.com\/\/common\/pet\/enterRoom\/h5\?invitePin=.*(&inviteSource=task_invite&shareSource=\w+&inviteTimeStamp=\d+&openId=\w+)?&reqSource=weapp|^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/pet\/helpFriend\?friendPin url script-request-header jd_joy_help.js
+
 =====================Loon=====================
 [Script]
 http-request ^https:\/\/draw\.jdfcloud\.com\/\/common\/pet\/enterRoom\/h5\?invitePin=.*(&inviteSource=task_invite&shareSource=\w+&inviteTimeStamp=\d+&openId=\w+)?&reqSource=weapp|^https:\/\/draw\.jdfcloud\.com(\/mirror)?\/\/pet\/helpFriend\?friendPin script-path=jd_joy_help.js, requires-body=true, timeout=3600, tag=宠汪汪强制为别人助力
+
+
 你也可从下面链接拿好友的friendPin(复制链接到有京东ck的浏览器打开即可)
+
 https://jdjoy.jd.com/pet/getFriends?itemsPerPage=20&currentPage=1
 */
 const friendsArr = []
