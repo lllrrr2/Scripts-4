afe1bccd19fa8bf0db0fa8753ab5fc189b66bee2
acoolbook <72637491+acoolbook@users.noreply.github.com>
2021-12-07 21:27:00 +0800

    Update jd_split.js

--- jd_split.js
+++ jd_split.js
@@ -65,13 +65,13 @@ $.shareCodes = [];
       await jdSplit()
     }
   }
-  let res = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/split.json')
-  if (!res) {
+  let res = await getAuthorShareCode('')
+  if (res) {
     $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/split.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
     res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/split.json')
   }
-  $.newShareCodes = [...new Set([...$.shareCodes, ...[]])]
+  //$.newShareCodes = [...new Set([...$.shareCodes, ...[]])]
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
     $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
