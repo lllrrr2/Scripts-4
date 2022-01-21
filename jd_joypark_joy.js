420f07e7d0664d2ceae5b845639c0a82be45eb7d
acoolbook <72637491+acoolbook@users.noreply.github.com>
2022-01-21 18:24:57 +0800

    Update jd_joypark_joy.js

--- jd_joypark_joy.js
+++ jd_joypark_joy.js
@@ -43,7 +43,7 @@ if ($.isNode()) {
 }
 
 //最大化硬币收益模式
-$.JOY_COIN_MAXIMIZE = process.env.JOY_COIN_MAXIMIZE === '1'
+$.JOY_COIN_MAXIMIZE = true
 $.log(`最大化收益模式: 已${$.JOY_COIN_MAXIMIZE ? `默认开启` : `关闭`}  `)
 
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
@@ -352,9 +352,9 @@ function doJoyMerge(joyId1, joyId2) {
         } else {
           data = JSON.parse(data);
           $.log(`合成 ${joyId1} <=> ${joyId2} ${data.success ? `成功！` : `失败！【${data.errMsg}】 code=${data.code}`}`)
-          if (data.code == '1006') {
-            hotFlag = true
-          }
+          // if (data.code == '1006') {
+          //   hotFlag = true
+          // }
         }
       } catch (e) {
         $.logErr(e, resp)
@@ -503,7 +503,7 @@ function apCashWithDraw(id, poolBaseId, prizeGroupId, prizeBaseId) {
 function getShareCode() {
   return new Promise(resolve => {
       $.get({
-          url: "",
+          url: "https://raw.githubusercontent.com/KingRan/JD-Scripts/main/shareCodes/joypark.json",
           headers: {
               "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
           }
