3cc25b2a987cb38ee20ca204715a1f496a989df0
acoolbook <72637491+acoolbook@users.noreply.github.com>
2022-03-28 20:17:57 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -283,7 +283,7 @@ function getSignfromPanda(functionId, body) {
 		        "accept-encoding": "gzip, deflate, br",
 		        'Content-Type': 'application/json',
 		    },
-		    timeout: 30000
+		    timeout: 10000
         }
         $.post(url, async(err, resp, data) => {
             try {				
@@ -423,7 +423,7 @@ function taskUrl(functionId, body = {}) {
 function getAuthorShareCode(url) {
   return new Promise(resolve => {
     const options = {
-      url: `${url}?${new Date()}`, "timeout": 30000, headers: {
+      url: `${url}?${new Date()}`, "timeout": 3000, headers: {
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }
     };
