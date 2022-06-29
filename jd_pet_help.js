12a5eb438c6422f606d017b80adedea06adfb01a
acoolbook <72637491+acoolbook@users.noreply.github.com>
2022-06-29 20:17:38 +0800

    Update jd_pet_help.js

--- jd_pet_help.js
+++ jd_pet_help.js
@@ -195,7 +195,7 @@ async function jdPet() {
         const errMsg = `京东账号${$.index} ${$.nickName || $.UserName}\n任务执行异常，请检查执行日志 ‼️‼️`;
         if ($.isNode())
             //await notify.sendNotify(`${$.name}`, errMsg);
-        $.msg($.name, '', `${errMsg}`)
+        //$.msg($.name, '', `${errMsg}`)
     }
 }
 
