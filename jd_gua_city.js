718db5c2fb72cc2a578699b89c8af93461ba7cfc
acoolbook <72637491+acoolbook@users.noreply.github.com>
2022-12-29 22:52:32 +0800

    Update jd_gua_city.js

--- jd_gua_city.js
+++ jd_gua_city.js
@@ -14,7 +14,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 let exchangeFlag = $.getdata('JD_CITY_EXCHANGE') || "true";//是否开启自动抽奖，建议活动快结束开启，默认关闭
 exchangeFlag = $.isNode() ? (process.env.JD_CITY_EXCHANGE ? process.env.JD_CITY_EXCHANGE : `${exchangeFlag}`) : ($.getdata('JD_CITY_EXCHANGE') ? $.getdata('JD_CITY_EXCHANGE') : `${exchangeFlag}`);
 // 优先助力[助力池]
-let helpShareFlag = "false";//是否优先助力[助力池]，默认是
+let helpShareFlag = "true";//是否优先助力[助力池]，默认是
 helpShareFlag = $.isNode() ? (process.env.JD_CITY_HELPSHARE ? process.env.JD_CITY_HELPSHARE : `${helpShareFlag}`) : ($.getdata('JD_CITY_HELPSHARE') ? $.getdata('JD_CITY_HELPSHARE') : `${helpShareFlag}`);
 $.whitelist = "";// 优先获取助力码的ckPin 用&隔开 pin值(填中文
 $.whitelist = $.isNode() ? (process.env.JD_CITY_WHITELIST ? process.env.JD_CITY_WHITELIST : `${$.whitelist}`) : ($.getdata('JD_CITY_WHITELIST') ? $.getdata('JD_CITY_WHITELIST') : `${$.whitelist}`);
@@ -84,8 +84,9 @@ $.token = process.env.gua_log_token || token // token
             $.index = i + 1;
             await getUA()
             await getInviteId();
-            if($.index >= (10 + $.collectAllCount)) {
-                console.log(`已获取超过10个`)
+            await $.wait(3000)
+            if($.index >= (5 + $.collectAllCount)) {
+                console.log(`已获取超过5个`)
                 break
             }
         }
@@ -151,7 +152,7 @@ $.token = process.env.gua_log_token || token // token
                     isLogin = false
                     break
                 }
-                // await $.wait(3000)
+                await $.wait(3000)
             }
             if(!isLogin){
                 continue
@@ -207,7 +208,7 @@ function taskPostUrl(functionId, body) {
         url: `${JD_API_HOST}`,
         body: `functionId=${functionId}&appid=signed_wh5&body=${(JSON.stringify(body))}&client=wh5&clientVersion=1.0.0`,
         headers: {
-            'Cookie': cookie,
+            'Cookie': ($.abcv ? $.abcv : "")+cookie,
             'Connection': 'keep-alive',
             'Content-Type': 'application/x-www-form-urlencoded',
             "User-Agent": $.UA,
